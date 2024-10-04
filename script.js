d64c74fda4d302f42aee1980d7f39c738d7c2c28
98.danya <1998.danyamohammed@gmail.com>
2024-10-04 22:42:34 +0200

    Fixed the issues
    Fixed case-insensitivity, # of rounds that should be played and the computer choice finally being displayed

--- script.js
+++ script.js
@@ -1,56 +1,53 @@
 function getComputerChoice() {
-    const randomNum = Math.floor(Math.random() * 3);
-    switch (randomNum) {
+    const randomChoice = Math.floor(Math.random() * 3);
+    switch (randomChoice) {
         case 0:
-            return "Rock";
+            return "rock";
         case 1:
-            return "Paper";
+            return "paper";
         case 2:
-            return "Scissors";
+            return "scissors";
     }
 }
 
 function getHumanChoice() {
-    const userInput = prompt("Rock, Paper or Scissors? What is your choice?");
-    if (userInput === "Rock" || userInput === "Paper" || userInput === "Scissors") {
-        return console.log("Your choice: " + userInput);
+    const userInput = prompt("Rock, Paper or Scissors? What is your choice?").toLowerCase();
+    if (userInput === "rock" || userInput === "paper" || userInput === "scissors") {
+        return userInput;
     } else {
         console.log("Your choice is invalid. Try again!");
+        return null;
     }
 }
 
 function playGame() {
-    var humanScore = 0;
-    var computerScore = 0;
+    let humanScore = 0;
+    let computerScore = 0;
     
     function playRound(humanChoice, computerChoice) {
         if (humanChoice === computerChoice) {
             return console.log("It's a tie. Try again!");
-        } else if (humanChoice === "Scissors" && computerChoice === "Paper") {
+        } else if (humanChoice === "scissors" && computerChoice === "paper") {
             humanScore++;
-            return console.log("Your Choice: " + humanChoice + "The Computer's Choice: " + computerChoice + "/nCongrats! You won this round. Your current score: " + humanScore + " And the computer's score: " + computerScore);
-        } else if (humanChoice === "Paper" && computerChoice === "Rock") {
+            return console.log("Your Choice: " + humanChoice + " The Computer's Choice: " + computerChoice + "\nCongrats! You won this round." + "\nYour current score: " + humanScore + " And the computer's score: " + computerScore);
+        } else if (humanChoice === "paper" && computerChoice === "rock") {
             humanScore++;
-            return console.log("Your Choice: " + humanChoice + "The Computer's Choice: " + computerChoice + "/nCongrats! You won this round. Your current score: " + humanScore + " And the computer's score: " + computerScore);
-        } else if (humanChoice === "Rock" && computerChoice === "Scissors") {
+            return console.log("Your Choice: " + humanChoice + " The Computer's Choice: " + computerChoice + "\nCongrats! You won this round." + "\nYour current score: " + humanScore + " And the computer's score: " + computerScore);
+        } else if (humanChoice === "rock" && computerChoice === "scissors") {
             humanScore++;
-            return console.log("Your Choice: " + humanChoice + "The Computer's Choice: " + computerChoice + "/nCongrats! You won this round. Your current score: " + humanScore + " And the computer's score: " + computerScore);
+            return console.log("Your Choice: " + humanChoice + " The Computer's Choice: " + computerChoice + "\nCongrats! You won this round." + "\nYour current score: " + humanScore + " And the computer's score: " + computerScore);
         } else {
             computerScore++;
-            return console.log("You lost. Better luck next time! Your current score: " + humanScore + " And the computer's score: " + computerScore);
+            return console.log("Your Choice: " + humanChoice + " The Computer's Choice: " + computerChoice + "\nYou lost. Better luck next time!" + "\nYour current score: " + humanScore + " And the computer's score: " + computerScore);
         }
     } 
-    const humanSelection = getHumanChoice();
-    const computerSelection = getComputerChoice();
-    
-    console.log(playRound(humanSelection, computerSelection));
+
+    for (let i = 0; i < 5; i++) {
+        const humanSelection = getHumanChoice();
+        const computerSelection = getComputerChoice();
+        playRound(humanSelection, computerSelection);
+    }
 }
 
-console.log(playGame());
-
-
-// Problems that need to be fixed!
-// 1. Human Choice is not case-insensitive
-// 2. Only one round is being played. Five rounds should be played!
-// (3. This is minimal: But the computer's choice is not being displayed!)
+playGame();
 
